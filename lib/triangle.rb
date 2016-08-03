bcffc427ce99428857443ff3af2a5855a20296ed
Alison <alisonhmullin@gmail.com>
2016-08-03 18:44:24 -0400

    define custom triangle error

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+
+attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2 || side1 + side2 + side3 == 0
+      begin
+        raise TriangleError
+        end
+    elsif side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 != side2 && side2 != side3 && side1 != side3
+      :scalene
+    else
+      :isosceles
+    end
+  end
+end
+
+class TriangleError < StandardError
+  "Not a valid triangle"
 end
